1f0bcb6dd84c60dc9b2273170ceffac95de5322c
João Santos <joaosantos@MacBook-Pro-Design-2.local>
2022-06-26 09:33:38 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Button from './Button';
+import Download from './Download';
+import SectionWrapper from './SectionWrapper';
+import Features from './Features';
+
+export { Features, Button, Download, SectionWrapper}
